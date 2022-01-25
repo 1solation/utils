011304ebbdae78faf5543977a0f5bfe8e0b025dd
Hasib Ahmed <hasibahmed1996@gmail.com>
2022-01-25 22:44:13 +0000

    update setup commands

--- mac/setup-commands.sh
+++ mac/setup-commands.sh
@@ -9,8 +9,10 @@ brew install --cask alfred # remove spotlight keyboard shortcut and set CMD+spac
 brew install --cask firefox
 # install nvm/node
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+# reload terminal, may get nvm is not a command error
+# if above error occurs if in zsh, cd ~, vi .zshrc and add in source lines from snippet in readme located at https://github.com/nvm-sh/nvm 
 nvm install stable
 mkdir ~/workspace
-npm install -g lite-server eslint
+npm install -g eslint
 brew install --cask visual-studio-code
 # update vscode settings & install extensions 
